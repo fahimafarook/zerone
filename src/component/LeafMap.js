9251fc1ba92750a143405b53efed997788607ef7
fahimafarook <52258914+fahimafarook@users.noreply.github.com>
2023-10-31 10:34:16 +0530

    Update LeafMap.js

--- src/component/LeafMap.js
+++ src/component/LeafMap.js
@@ -1,27 +1,27 @@
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useState } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { useState } from 'react';
 
 import React from 'react'
 
 function LeafMap() {
-  const [center, setCenter] = useState([11.2359071, 78.8386118]);
-  const [zoom, setZoom] = useState(15);
+  // const [center, setCenter] = useState([11.2359071, 78.8386118]);
+  // const [zoom, setZoom] = useState(15);
   return (
     <div>
-      <div>
-        <MapContainer center={center} zoom={zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={center}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
+      // <div>
+      //   <MapContainer center={center} zoom={zoom}>
+      //     <TileLayer
+      //       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      //       url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+      //     />
+      //     <Marker position={center}>
+      //       <Popup>
+      //         A pretty CSS3 popup. <br /> Easily customizable.
+      //       </Popup>
+      //     </Marker>
+      //   </MapContainer>
+      // </div>
     </div>
   )
 }
