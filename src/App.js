b53740752196eb43538de6041e77af8577c286c1
fahimafarook <52258914+fahimafarook@users.noreply.github.com>
2023-11-01 07:21:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import LandingPage from './component/LandingPage.js'
 import PricingPage from './component/PricingPage.js'
 import Aboutus from './component/Aboutus';
 import Form from './component/Form';
-import LocationComponent from './component/LocationComponent'
-import MapComponent from './component/MapComponent'
-import OpenLayersMap from './component/Map';
-import LeafMap from './component/LeafMap';
-import ReverseGeocoding from './component/ReverseGeocoding';
+// import LocationComponent from './component/LocationComponent'
+// import MapComponent from './component/MapComponent'
+// import OpenLayersMap from './component/Map';
+// import LeafMap from './component/LeafMap';
+// import ReverseGeocoding from './component/ReverseGeocoding';
 import FooterComponent from './component/footer';
 
 export const DeviceContext = React.createContext();
